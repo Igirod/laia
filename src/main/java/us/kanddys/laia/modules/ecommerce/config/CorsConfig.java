7abcb4826df5429084453efbe1beb963149cf311
Igirod <ignaciogirodsd@gmail.com>
2024-02-19 14:27:11 -0300

    Fix Cors Policy Graphql - Complete

--- src/main/java/us/kanddys/laia/modules/ecommerce/config/CorsConfig.java
+++ src/main/java/us/kanddys/laia/modules/ecommerce/config/CorsConfig.java
@@ -11,7 +11,13 @@ public class CorsConfig implements WebMvcConfigurer {
    public void addCorsMappings(CorsRegistry registry) {
       registry.addMapping("/**")
             .allowedOrigins("http://localhost:4200")
-            .allowedMethods("GET", "POST", "PUT", "DELETE") 
-            .allowedHeaders("*"); 
+            .allowedMethods("GET", "POST", "PUT", "DELETE")
+            .allowedHeaders("*");
+            
+      // Permitir solicitudes CORS para la ruta de GraphQL
+      registry.addMapping("/graphql")
+            .allowedOrigins("http://localhost:4200")
+            .allowedMethods("POST") // Por lo general, las solicitudes GraphQL son POST
+            .allowedHeaders("*");
    }
 }
