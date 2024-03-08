070db7b679bb6746c4e182baed120e5d2728e382
Igirod <ignaciogirodsd@gmail.com>
2024-03-08 14:39:13 -0300

    Fix update orders atributes repository - Complete

--- src/main/java/us/kanddys/laia/modules/ecommerce/repository/OrderJpaRepository.java
+++ src/main/java/us/kanddys/laia/modules/ecommerce/repository/OrderJpaRepository.java
@@ -12,11 +12,11 @@ import us.kanddys.laia.modules.ecommerce.model.Order;
 public interface OrderJpaRepository extends JpaRepository<Order, Long> {
 
    @Modifying
-   @Query(value = "UPDATE Orders SET status = ?2 WHERE id = ?1", nativeQuery = true)
+   @Query(value = "UPDATE orders SET status = ?2 WHERE id = ?1", nativeQuery = true)
    void updateStatus(Long id, String status);
 
    @Modifying
-   @Query(value = "UPDATE Orders SET note = ?2 WHERE id = ?1", nativeQuery = true)
+   @Query(value = "UPDATE orders SET note = ?2 WHERE id = ?1", nativeQuery = true)
    void updateNote(Long id, String note);
 
    @Query(value = "SELECT MAX(id) FROM orders WHERE mer_id = :merchantId AND status IN ('PENDING', 'COMPLETE')", nativeQuery = true)
