50441f8dea025dc2d86fdfaefd807e4f5e257986
Igirod <ignaciogirodsd@gmail.com>
2024-03-21 14:56:17 -0300

    Add https://kanddys-1088e.web.app in CorsPolicy

--- src/main/java/us/kanddys/laia/modules/ecommerce/config/CorsConfig.java
+++ src/main/java/us/kanddys/laia/modules/ecommerce/config/CorsConfig.java
@@ -21,21 +21,21 @@ public class CorsConfig implements WebMvcConfigurer {
       // * Permitir solicitudes CORS para todas las rutas
       registry.addMapping("/**")
             .allowedOrigins("http://localhost:4200", "https://laia-development.netlify.app",
-                  "https://laia-dev.up.railway.app", "https://www.flores.club/")
+                  "https://laia-dev.up.railway.app", "https://www.flores.club/", "https://kanddys-1088e.web.app")
             .allowedMethods("GET", "POST", "PUT", "DELETE")
             .allowedHeaders("*");
 
       // * Permitir solicitudes CORS para la ruta de GraphQL
       registry.addMapping("/graphql")
             .allowedOrigins("http://localhost:4200", "https://laia-dev.up.railway.app",
-                  "https://laia-development.netlify.app", "https://www.flores.club")
+                  "https://laia-development.netlify.app", "https://www.flores.club", "https://kanddys-1088e.web.app")
             .allowedMethods("POST", "GET") // ! Por lo general, las solicitudes GraphQL son POST
             .allowedHeaders("*");
 
       // * Permitir solicitudes CORS para la URL de GraphiQL
       registry.addMapping("/graphiql")
             .allowedOrigins("http://localhost:4200", "https://laia-dev.up.railway.app",
-                  "https://laia-development.netlify.app", "https://www.flores.club")
+                  "https://laia-development.netlify.app", "https://www.flores.club", "https://kanddys-1088e.web.app")
             .allowedMethods("GET", "POST") // ! Por lo general, GraphiQL usa solicitudes GET
             .allowedHeaders("*");
    }
