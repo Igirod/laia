7d4151f2d22db49f8d346eeb5fee95d4e8b49911
Igirod <ignaciogirodsd@gmail.com>
2024-03-25 14:50:27 -0300

    Definition of context for AI

--- src/main/java/us/kanddys/laia/modules/ecommerce/services/impl/AIServiceImpl.java
+++ src/main/java/us/kanddys/laia/modules/ecommerce/services/impl/AIServiceImpl.java
@@ -5,13 +5,8 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-
-import us.kanddys.laia.modules.ecommerce.model.AImessage;
 import us.kanddys.laia.modules.ecommerce.services.AIService;
 
 @Service
@@ -25,25 +20,9 @@ public class AIServiceImpl implements AIService {
 
    @Override
    public String askNewQuestion(String question) {
-      List<AImessage> messages = new ArrayList<AImessage>();
-      // messages.add(new AImessage("system",
-      // "[Sos un asistente que trabaja en una biblioteca de articulos y tu tarea es
-      // interpretar que acción quiere llevar a cabo el usuario], [Cualquier pregunta
-      // que no este relacionada a los articulos, respondes: Unicamente respondo
-      // preguntas relacionadas a la biblioteca de articulos], [Respuesta debe ser una
-      // sola palabra clave], [Respuestas: ORDER-LOW-STOCK, ORDER-HIGHER-STOCK,
-      // ORDER-STOCK-BESTSELLERS, ORDER-STOCK-LESSSOLD, ORDER-STOCK-CHEAP,
-      // ORDER-STOCK-EXPENSIVE]"));
-
-      StringBuilder sb = new StringBuilder();
-      sb.append({"role": "user", "content": "Say this is a test!"})
-      
-      messages.add(new AImessage("user", "Bajo stock?"));
-      messages.add(new AImessage("assistant", "ORDER-LOW-STOCK"));
-      System.out.println(messages.toString());
       HttpResponse<String> response = null;
-      String requestBody = "{\"model\": \"gpt-3.5-turbo\", \"messages\": [" + messages.toString()
-            + ",{\"role\": \"user\", \"content\": \""
+      String requestBody = "{\"model\": \"gpt-3.5-turbo\", \"messages\": [{\"role\": \"user\", \"content\": \""
+            + "[You are an assistant working in an article library and your task is to interpret what action the user wants to perform], [Any question that is not related to the articles, you answer: I only answer questions related to the article library], [Answer: one or more keywords depending on the user's intention], [Answers: ORDER-LOW-STOCK, ORDER-HIGH-STOCK, ORDER-STOCK-BESTSELLERS, ORDER-STOCK-LESSSOLD, ORDER-STOCK-CHEAP, ORDER-STOCK-EXPENSIVE]]"
             + question + "\"}], \"temperature\": 0.7}";
       HttpClient httpClient = HttpClient.newBuilder()
             .version(HttpClient.Version.HTTP_2)
