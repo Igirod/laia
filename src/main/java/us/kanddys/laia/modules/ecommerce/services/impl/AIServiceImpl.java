caca980092006757cb5001f6f50d8c0108bf2471
Igirod <ignaciogirodsd@gmail.com>
2024-03-26 16:07:53 -0300

    Test multiple V - Complete

--- src/main/java/us/kanddys/laia/modules/ecommerce/services/impl/AIServiceImpl.java
+++ src/main/java/us/kanddys/laia/modules/ecommerce/services/impl/AIServiceImpl.java
@@ -22,12 +22,13 @@ public class AIServiceImpl implements AIService {
    public String askNewQuestion(String question) {
       HttpResponse<String> response = null;
       String requestBody = "{\"model\": \"gpt-3.5-turbo\", \"messages\": [{\"role\": \"user\", \"content\": \""
-            + "[Sos un asistente que trabaja en una biblioteca de artículos y tu tarea es interpretar qué acción quiere llevar a cabo el usuario], [Cualquier pregunta que no esté relacionada a los artículos, respondes: Únicamente respondo preguntas relacionadas a la biblioteca de articulos], [Respuesta: una o más palabras claves segun la intención del usuario], [Respuestas: ORDER-LOW-STOCK (Ordenar articulos por stock ascendente), ORDER-HIGH-STOCK (Ordenar articulos por stock descendente), ORDER-STOCK-BESTSELLERS (Ordenar articulos por ventas descendente), ORDER-STOCK-LESSSOLD (Ordenar articulos por venta ascendente), ORDER-STOCK-CHEAP (Ordenar articulos por precio ascendente), ORDER-STOCK-EXPENSIVE (Ordenar articulos por precio descendente)]]"
+            + "[Sos un asistente amigable que trabaja en una biblioteca de artículos y tu tarea es interpretar qué acción quiere llevar a cabo el usuario. Analizás palabra por palabra la oración para brindar una respuesta correcta, teniendo en cuenta la ubicación de las órdenes y la intención del usuario], [Cualquier pregunta que no esté relacionada a los artículos, respondes: Únicamente respondo preguntas relacionadas a la biblioteca de articulos], [Respuesta: una o más palabras claves según la intención del usuario separadas por coma], [Respuestas: ORDER-STOCK-LOW-QUANTITY (Ordenar artículos por cantidad ascendente, ejemplo: ordenar artículos por menor cantidad), ORDER-STOCK-HIGH-QUANTITY (Ordenar artículos por cantidad descendente, ejemplo: ordenar artículos por mayor cantidad), ORDER-BESTSELLERS (Ordenar artículos por ventas descendente), ORDER-LESSSOLD (Ordenar artículos por ventas ascendente), ORDER-PRICE-CHEAP (Ordenar artículos por precio ascendente), ORDER-PRICE-EXPENSIVE (Ordenar artículos por precio descendente), CREATE-ARTICLE (Crear un artículo), DELETE-ARTICLE (Eliminar un artículo), UPDATE-ARTICLE (Modificar un artículo)]"
             + question + "\"}], \"temperature\": 0.7}";
       HttpClient httpClient = HttpClient.newBuilder()
             .version(HttpClient.Version.HTTP_2)
             .build();
       HttpRequest request = HttpRequest.newBuilder()
+
             .uri(URI.create(openAIApiUrl))
             .POST(HttpRequest.BodyPublishers.ofString(requestBody))
             .header("Content-Type", "application/json")
